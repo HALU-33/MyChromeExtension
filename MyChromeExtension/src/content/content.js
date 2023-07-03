ed5d95d6bb08b44459b89de3291b087e9c4f1bca
HALU_33 <gamer.halu.33@gmail.com>
2023-07-03 11:02:20 +0900

    update files

--- MyChromeExtension/src/content/content.js
+++ MyChromeExtension/src/content/content.js
@@ -1,17 +1,24 @@
 var scrollSpeed, refreshInterval; // スクロール速度と更新間隔を格納する変数を宣言
+var scrollEnabled, refreshEnabled; // スクロールと更新のオン・オフ状態を格納する変数を宣言
 
-// ストレージからスクロール速度と更新間隔を取得
-chrome.storage.sync.get(['scrollSpeed', 'refreshInterval'], function(result) {
+// ストレージから設定値を取得
+chrome.storage.sync.get(['scrollSpeed', 'refreshInterval', 'scrollEnabled', 'refreshEnabled'], function(result) {
     scrollSpeed = result.scrollSpeed;
     refreshInterval = result.refreshInterval;
+    scrollEnabled = result.scrollEnabled;
+    refreshEnabled = result.refreshEnabled;
 });
 
-// 1秒ごとにページを指定したピクセルだけスクロールするように設定
+// 1秒ごとにページを指定したピクセルだけスクロールするように設定（スクロールが有効な場合のみ）
 setInterval(function() {
-    window.scrollBy(0, scrollSpeed);
+    if (scrollEnabled) {
+        window.scrollBy(0, scrollSpeed);
+    }
 }, 1000);
 
-// 指定した間隔でページをリロードするように設定
+// 指定した間隔でページをリロードするように設定（リロードが有効な場合のみ）
 setInterval(function() {
-    location.reload();
+    if (refreshEnabled) {
+        location.reload();
+    }
 }, refreshInterval * 60 * 1000);
--- MyChromeExtension/src/popup/popup.js
+++ MyChromeExtension/src/popup/popup.js
@@ -1,11 +1,33 @@
 // 保存ボタンにクリックイベントリスナーを追加
 document.getElementById('save').addEventListener('click', function() {
-  // スクロール速度と更新間隔の入力フィールドから値を取得
+  // チェックボックスと数値入力フィールドから値を取得
+  var scrollEnabled = document.getElementById('scrollEnabled').checked;
+  var refreshEnabled = document.getElementById('refreshEnabled').checked;
   var scrollSpeed = document.getElementById('scrollSpeed').value;
   var refreshInterval = document.getElementById('refreshInterval').value;
 
-  // 取得した値をストレージに保存するように設定
-  chrome.storage.sync.set({scrollSpeed: scrollSpeed, refreshInterval: refreshInterval}, function() {
+  // 取得した値をストレージに保存
+  chrome.storage.sync.set({
+    scrollEnabled: scrollEnabled,
+    refreshEnabled: refreshEnabled,
+    scrollSpeed: scrollSpeed,
+    refreshInterval: refreshInterval
+  }, function() {
     console.log('Settings saved'); // 設定が保存されたことをコンソールに出力
   });
 });
+
+// ページロード時にストレージから設定を読み込む
+window.onload = function() {
+  chrome.storage.sync.get(['scrollEnabled', 'refreshEnabled', 'scrollSpeed', 'refreshInterval'], function(result) {
+    // ストレージから取得した設定値をフォームに適用
+    document.getElementById('scrollEnabled').checked = result.scrollEnabled;
+    document.getElementById('refreshEnabled').checked = result.refreshEnabled;
+    document.getElementById('scrollSpeed').value = result.scrollSpeed;
+    document.getElementById('refreshInterval').value = result.refreshInterval;
+
+    // チェックボックスの状態に応じて数値入力フィールドの有効・無効を設定
+    document.getElementById('scrollSpeed').disabled = !result.scrollEnabled;
+    document.getElementById('refreshInterval').disabled = !result.refreshEnabled;
+  });
+};
