e77c6bbca24f97e414a922c31bc492e19633d15b
HALU_33 <gamer.halu.33@gmail.com>
2023-07-03 19:15:23 +0900

    add options and update popup

--- MyChromeExtension/src/popup/popup.js
+++ MyChromeExtension/src/popup/popup.js
@@ -1,43 +1,24 @@
-// 拡張機能がインストールされたときに初期設定を保存
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({
-    scrollEnabled: false,
-    refreshEnabled: false,
-    scrollSpeed: 0,
-    refreshInterval: 0
-  });
+// スクロールのトグルボタンにイベントリスナーを追加
+document.getElementById('toggleScroll').addEventListener('change', function() {
+  // トグルボタンの状態を取得
+  var scrollEnabled = document.getElementById('toggleScroll').checked;
+  // ストレージにトグルボタンの状態を保存
+  chrome.storage.sync.set({scrollEnabled: scrollEnabled});
 });
 
-// 保存ボタンにクリックイベントリスナーを追加
-document.getElementById('save').addEventListener('click', function() {
-  // チェックボックスと数値入力フィールドから値を取得
-  var scrollEnabled = document.getElementById('scrollEnabled').checked;
-  var refreshEnabled = document.getElementById('refreshEnabled').checked;
-  var scrollSpeed = document.getElementById('scrollSpeed').value;
-  var refreshInterval = document.getElementById('refreshInterval').value;
-
-  // 取得した値をストレージに保存
-  chrome.storage.sync.set({
-    scrollEnabled: scrollEnabled,
-    refreshEnabled: refreshEnabled,
-    scrollSpeed: scrollSpeed,
-    refreshInterval: refreshInterval
-  }, function() {
-    console.log('Settings saved'); // 設定が保存されたことをコンソールに出力
-  });
+// リフレッシュのトグルボタンにイベントリスナーを追加
+document.getElementById('toggleRefresh').addEventListener('change', function() {
+  // トグルボタンの状態を取得
+  var refreshEnabled = document.getElementById('toggleRefresh').checked;
+  // ストレージにトグルボタンの状態を保存
+  chrome.storage.sync.set({refreshEnabled: refreshEnabled});
 });
 
 // ページロード時にストレージから設定を読み込む
 window.onload = function() {
-  chrome.storage.sync.get(['scrollEnabled', 'refreshEnabled', 'scrollSpeed', 'refreshInterval'], function(result) {
-    // ストレージから取得した設定値をフォームに適用
-    document.getElementById('scrollEnabled').checked = result.scrollEnabled;
-    document.getElementById('refreshEnabled').checked = result.refreshEnabled;
-    document.getElementById('scrollSpeed').value = result.scrollSpeed || '';
-    document.getElementById('refreshInterval').value = result.refreshInterval || '';
-
-    // チェックボックスの状態に応じて数値入力フィールドの有効・無効を設定
-    document.getElementById('scrollSpeed').disabled = !result.scrollEnabled;
-    document.getElementById('refreshInterval').disabled = !result.refreshEnabled;
+  chrome.storage.sync.get(['scrollEnabled', 'refreshEnabled'], function(result) {
+      // ストレージから取得した設定値をトグルボタンに適用
+      document.getElementById('toggleScroll').checked = result.scrollEnabled;
+      document.getElementById('toggleRefresh').checked = result.refreshEnabled;
   });
 };
