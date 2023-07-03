df29c34b3c0b66540e1331fb9b4ef27b53db4618
HALU_33 <gamer.halu.33@gmail.com>
2023-07-03 20:11:30 +0900

    update popup and option

--- MyChromeExtension/src/popup/popup.js
+++ MyChromeExtension/src/popup/popup.js
@@ -1,24 +1,27 @@
-// スクロールのトグルボタンにイベントリスナーを追加
-document.getElementById('toggleScroll').addEventListener('change', function() {
-  // トグルボタンの状態を取得
-  var scrollEnabled = document.getElementById('toggleScroll').checked;
-  // ストレージにトグルボタンの状態を保存
-  chrome.storage.sync.set({scrollEnabled: scrollEnabled});
-});
-
-// リフレッシュのトグルボタンにイベントリスナーを追加
-document.getElementById('toggleRefresh').addEventListener('change', function() {
-  // トグルボタンの状態を取得
-  var refreshEnabled = document.getElementById('toggleRefresh').checked;
-  // ストレージにトグルボタンの状態を保存
-  chrome.storage.sync.set({refreshEnabled: refreshEnabled});
-});
-
-// ページロード時にストレージから設定を読み込む
-window.onload = function() {
-  chrome.storage.sync.get(['scrollEnabled', 'refreshEnabled'], function(result) {
-      // ストレージから取得した設定値をトグルボタンに適用
+document.addEventListener('DOMContentLoaded', function() {
+  // ページロード時にストレージから設定を読み込む
+  chrome.storage.sync.get(['scrollEnabled', 'refreshEnabled', 'scrollSpeed', 'refreshInterval'], function(result) {
       document.getElementById('toggleScroll').checked = result.scrollEnabled;
       document.getElementById('toggleRefresh').checked = result.refreshEnabled;
+
+      // デフォルトのスクロールスピードと更新間隔を設定
+      if (result.scrollSpeed === undefined) {
+          chrome.storage.sync.set({scrollSpeed: 50}); // デフォルトのスクロールスピードを50に設定
+      }
+      if (result.refreshInterval === undefined) {
+          chrome.storage.sync.set({refreshInterval: 1}); // デフォルトの更新間隔を1分に設定
+      }
   });
-};
+
+  // 設定ボタンにイベントリスナーを追加
+  document.getElementById('applySettings').addEventListener('click', function() {
+    var scrollEnabled = document.getElementById('toggleScroll').checked;
+    var refreshEnabled = document.getElementById('toggleRefresh').checked;
+    chrome.storage.sync.set({scrollEnabled: scrollEnabled, refreshEnabled: refreshEnabled}, function() {
+        // 設定が保存されたら、ページを更新
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.reload(tabs[0].id);
+        });
+    });
+  });
+});
