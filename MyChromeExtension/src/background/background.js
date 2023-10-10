cffdf3fe8ba63f70493f9895efcd3745c4281fff
HALU_33 <gamer.halu.33@gmail.com>
2023-10-10 15:19:15 +0900

    add files

--- MyChromeExtension/src/background/background.js
+++
@@ -1,8 +0,0 @@
-// アクションアイコンがクリックされたときのリスナーを追加
-chrome.action.onClicked.addListener(function(tab) {
-    // content.jsを現在のタブで実行するように指示
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        files: ['src/content/content.js']
-    });
-});
